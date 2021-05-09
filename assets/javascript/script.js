5d21656ba44d19d455ac804cc0aeab8354328614
zoomzoom6 <tonyhuang107@live.ca>
2021-05-09 11:25:56 -0400

    Moved length and character prompts into separate functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,15 @@
 // Assignment code here
-var lwrCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var uprCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numSet = ['1','2','3','4','5','6','7','8','9','0'];
-var specChar = ['!','@','#','$','%','^','&','*','(',')',' ','`','~','?','<','>',';',':',',','.','[',']','{','}','"'];
+var lwrCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uprCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numSet = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ' ', '`', '~', '?', '<', '>', ';', ':', ',', '.', '[', ']', '{', '}', '"'];
 
 var useLwrCase = false;
 var useUprCase = false;
 var useNumSet = false;
 var useSpecChar = false;
-var lenPass = 8;
+var passWd = "";
+var passLen; 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -23,28 +24,38 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", function() {
-  var passLen = window.prompt("Enter the length of password you require: ");
+generateBtn.addEventListener("click", function () {
+  passLength();
+  charSetsUsed();
+
+  console.log(passLen);
+  console.log(useLwrCase);
+  console.log(useUprCase);
+  console.log(useNumSet);
+  console.log(useSpecChar);
+  //writePassword();
+});
+
+//Password length function
+function passLength() {
+  passLen = window.prompt("Enter the length of password you require: ");
   if (passLen < 8 || passLen === null || passLen > 128) {
     window.alert("Please enter a length of at least 8 and less than 128.");
-    passLen = window.prompt("Enter the length of password you require: ");
-    if (passLen < 8 || passLen === null || passLen > 128) {
-      window.alert("Please enter a length of at least 8 and less than 128. \nThis is your final warning!");
-      passLen = window.prompt("Enter the length of password you require: ");
-      if (passLen < 8 || passLen === null || passLen > 128) {
-        window.alert("This program will now shutdown. Goodbye");
-        return;
-      }
-    }
+    passLength();
+  } else {
+    return;
   }
+}
 
+//Characters to use function
+function charSetsUsed() {
   var confirmLwr = window.confirm("Do you want to include lower case characters? \nChoose OK for yes, Cancel for no.");
   if (confirmLwr) {
     useLwrCase = true;
   } else {
     useLwrCase = false;
   }
-  
+
   var confirmUpr = window.confirm("Do you want to include upper case characters? \nChoose OK for yes, Cancel for no.");
   if (confirmUpr) {
     useUprCase = true;
@@ -66,10 +77,10 @@ generateBtn.addEventListener("click", function() {
     useSpecChar = false;
   }
 
-  console.log(passLen);
-  console.log(useLwrCase);
-  console.log(useUprCase);
-  console.log(useNumSet);
-  console.log(useSpecChar);
-  //writePassword();
-});
+  if (confirmLwr === false && confirmUpr === false && confirmNum === false && confirmSpec === false) {
+    window.alert("Please choose to include at least one character set.");
+    charSetsUsed();
+  } else {
+    return;
+  }
+}
